788e3874e921c784c7b456ed4c279e6e1aee5cf9
Rico Meinl <rmeinl97@gmail.com>
2022-05-04 15:27:44 +0100

    Moved reusable tasks and functions into its own module so others can pip install it

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""setup.py"""
+from setuptools import setup
+
+
+setup(
+    name="bioinformatics_pipeline_tutorial",
+    version="0.0.1",
+    author="Rico Meinl",
+    author_email="dev@rmeinl.com",
+    description="This is the accompanying GitHub repository for this blog post: https://ricomnl.com/blog/bottom-up-bioinformatics-pipeline.",
+    license="MIT",
+    keywords="bioinformatics tutorial",
+    url="https://github.com/ricomnl/bioinformatics-pipeline-tutorial",
+    packages=["bioinformatics_pipeline_tutorial"],
+    install_requires=["redun", "plotly", "kaleido"],
+)
